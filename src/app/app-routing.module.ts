28c45283c8bdfe5d47ce0a34269e610d4f86cd18
dartho2 <wegrzyn_pawel@outlook.com>
2020-04-27 21:15:26 +0200

    home,about,gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './site/contact/contact.component';
 import { GalleryComponent } from './site/gallery/gallery.component';
 
 const routes: Routes = [
-
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuDishComponent },
   { path: 'contact', component: ContactComponent },
