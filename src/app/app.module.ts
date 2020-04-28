3b23627853d859a5060ddd21156dd0443f10693e
dartho2 <wegrzyn_pawel@outlook.com>
2020-04-28 20:16:47 +0200

    gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ContactComponent } from './site/contact/contact.component';
 import { GalleryComponent } from './site/gallery/gallery.component';
 import { NgxMasonryModule } from 'ngx-masonry';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryModule } from  '@ngx-gallery/core';
+import { LightboxModule } from  '@ngx-gallery/lightbox';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +25,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     GalleryComponent
   ],
   imports: [
+    GalleryModule,
+    LightboxModule.withConfig({
+      panelClass: 'fullscreen'
+    }),
     BrowserModule,
     BrowserAnimationsModule,
     NgxMasonryModule,
